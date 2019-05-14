35185d2747b06fbc84d232afb7da3b8bc093d35e
ttom921 <ttom921@hotmail.com>
2019-05-14 09:52:35 +0800

    加入測試程式

---
+++ eloquent-js-3e/chap02/chap02-1.js
@@ -0,0 +1,17 @@
+// 綁定
+// let ten = 10;
+// console.log(ten * ten);
+
+// let mood = 'light';
+// console.log(mood);
+// mood = 123;
+// console.log(mood);
+
+// 
+let printStr = '#';
+
+for (let i = 0; i < 7; i++) {
+
+    console.log(printStr);
+    printStr = printStr + '#';
+}
